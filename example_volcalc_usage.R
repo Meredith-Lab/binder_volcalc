6ea5407e0e646fc5fe5d018d0885127cd22ac8b1
Kristina Riemer <kristina.riemer@weecology.org>
2022-02-21 13:41:10 -0700

    Add ChemmineOB install and volcalc usage

--- example_volcalc_usage.R
+++ example_volcalc_usage.R
@@ -1,3 +1,7 @@
-library(KEGGREST)
+# run once per session to install volcalc, putting in your own GitHub PAT
+devtools::install_github("Meredith-Lab/binder_volcalc/",
+                         auth_token = "your_token")
 
-keggGet("C16181")
+# read in library and run a test function
+library(volcalc)
+calc_vol("map00361", "data", compound_id = "C16181")
--- install.R
+++ install.R
@@ -1,3 +1,4 @@
 install.packages("BiocManager")
 BiocManager::install("KEGGREST")
 BiocManager::install("ChemmineR")
+BiocManager::install("ChemmineOB")
